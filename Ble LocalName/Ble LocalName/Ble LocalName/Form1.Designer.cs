5aeb90e5723184576254962c360c959e5e590060
ZhangxileCN <105898373+ZhangxileCN@users.noreply.github.com>
2023-08-31 18:30:15 +0800

    Add files via upload

---
+++ Ble LocalName/Ble LocalName/Ble LocalName/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ble_LocalName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericEdit1 = new NationalInstruments.UI.WindowsForms.NumericEdit();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "搜索蓝牙";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.搜索蓝牙);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 118);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1840, 250);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(23, 428);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(1840, 250);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Keep only packets type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Get higher";
+            // 
+            // numericEdit1
+            // 
+            this.numericEdit1.Location = new System.Drawing.Point(365, 68);
+            this.numericEdit1.Name = "numericEdit1";
+            this.numericEdit1.Size = new System.Drawing.Size(120, 38);
+            this.numericEdit1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "清除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.清除);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1892, 1001);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericEdit1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericEdit1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private NationalInstruments.UI.WindowsForms.NumericEdit numericEdit1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ble LocalName/Ble LocalName/Ble LocalName/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Osram;
+using System.Threading;
+
+namespace Ble_LocalName
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Listview_init();
+        }
+
+        private void Listview_init()
+        {
+            listView1.Columns.Add("PacketType", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Address", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Count BT address bytes", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Rssi", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Data", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Payload", 100, HorizontalAlignment.Center);
+            listView1.View = View.Details;
+
+            listView2.Columns.Add("PacketType", 100, HorizontalAlignment.Center);
+            listView2.Columns.Add("Address", 100, HorizontalAlignment.Center);
+            listView2.Columns.Add("Count BT address bytes", 100, HorizontalAlignment.Center);
+            listView2.Columns.Add("Rssi", 100, HorizontalAlignment.Center);
+            listView2.Columns.Add("Data", 100, HorizontalAlignment.Center);
+            listView2.Columns.Add("Payload", 100, HorizontalAlignment.Center);
+            listView2.View = View.Details;
+
+        }
+
+        /**
+         *  查询listview列是否为空  * 
+         * */
+        bool IsColumnEmpty(ListView listView, int columnIndex)
+        {
+            foreach (ListViewItem item in listView.Items)
+            {
+                if (item.SubItems.Count > columnIndex && !string.IsNullOrEmpty(item.SubItems[columnIndex].Text))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void 搜索蓝牙(object sender, EventArgs e)
+        {
+
+            int higher1 = -100;
+            int _pt = (int)numericEdit1.Value;
+            listView1.Items.Clear();
+            Thread.Sleep(50);
+            listView2.Items.Clear();
+            Thread.Sleep(50);
+
+            //if (listView1.Items.Count > 0)
+            //    listView1.Items.RemoveAt(0);
+
+            //根据蓝牙发射器串口端口设定
+            BleManager.Instance.ComPort = "com3";
+            Thread.Sleep(500);
+            BleManager.Instance.StartDiscovery();
+            Thread.Sleep(5 * 1000);
+            BleManager.Instance.StopDiscovery();
+
+            Thread.Sleep(500);
+            Queue<BtDiscoveryEventPacket> queue = BleManager.Instance.DiscoveryPacketQueue;
+            foreach (BtDiscoveryEventPacket bt in queue)
+            {
+                string payload = null;
+                //string payloadascii = null;
+                if ((bt.Rssi >= -50 && bt.Rssi <= -20) && bt.PacketType == _pt)
+                {
+
+                    //higher = bt.Rssi;
+
+                    for (int i = 0; i < bt.PayLoad.Length; i++)
+                    {
+
+                        payload += bt.PayLoad[i].ToString();
+
+                    }
+                    Console.WriteLine("--------------------------------");
+                    Console.WriteLine($"PacketType: {bt.PacketType}");
+                    Console.WriteLine($"Address: {bt.Address}");
+                    Console.WriteLine($"Count BT address bytes: {bt.Address.ToString().Length - 11}");
+                    Console.WriteLine($"Rssi: {bt.Rssi}");
+                    Console.WriteLine($"Data: {bt.Data}");
+                    Console.WriteLine($"Payload: {payload}");
+
+                    //byte[] bytes = Encoding.ASCII.GetBytes(payload);
+                    //for (int i = 0; i < bytes.Length; i++)
+                    //{
+                    //    payloadascii += bytes[i];
+                    //}
+
+                    string[] textdata = { bt.PacketType.ToString(), bt.Address.ToString(), (bt.Address.ToString().Length - 11).ToString(), bt.Rssi.ToString(), bt.Data.ToString(), payload };
+                    ListViewItem item1 = new ListViewItem(textdata);
+                    listView1.Items.Add(item1);
+
+                    if (bt.Rssi > higher1)
+                    {
+                        string[] textdata1 = { bt.PacketType.ToString(), bt.Address.ToString(), (bt.Address.ToString().Length - 11).ToString(), bt.Rssi.ToString(), bt.Data.ToString(), payload };
+                        ListViewItem item2 = new ListViewItem(textdata1);
+                        listView2.Items.Clear();
+                        listView2.Items.Add(item2);
+                        higher1 = bt.Rssi;
+                    }
+
+                }
+
+            }
+            if (IsColumnEmpty(listView1, 0))
+                MessageBox.Show("未能找到WireLess BT", "提示");
+
+        }
+
+        private void 清除(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            Thread.Sleep(50);
+            listView2.Items.Clear();
+            Thread.Sleep(50);
+        }
+    }
+}
